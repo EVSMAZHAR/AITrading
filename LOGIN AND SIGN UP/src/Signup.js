3c3b678c50ae9a0d76f68c170fffa3dc09cb9d6e
SULAMAN ANWAR <Sulaman17#@>
2023-03-04 17:29:05 +0500

    first-commit

---
+++ LOGIN AND SIGN UP/src/Signup.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+function Signup() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </label>
+      <br />
+      <button type="submit">Signup</button>
+    </form>
+  );
+}
+
+export default Signup;
